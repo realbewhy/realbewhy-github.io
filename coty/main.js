a8b494722dd66229e4314ecc32f530be1f581f0e
B .Y <realbewhy@gmail.com>
2020-05-12 23:15:14 +0700

    coty

--- coty/main.js
+++ coty/main.js
@@ -17,29 +17,51 @@ window.onload = () => {
     //slide images
 
     //show function
-    const hiddenMenu = document.getElementById('hidden-menu')
+    // const hiddenMenu = document.getElementById('hidden-menu')
+    const profile = document.getElementById('profile')
+    const menu = document.getElementById('menu')
+
     document.getElementById("close").onclick = () => {
-        document.getElementById('main').style.display = 'flex'
-        document.getElementById('content').style.display = 'none'
-        document.getElementById('info-content').style.display = 'none'
-        hiddenMenu.style.display = 'none'
+        profile.style.display = 'none'
+        menu.style.display = 'flex'
     }
-    
+
     handleClick = (e) => {
-        console.log(e)
-        document.getElementById('main').style.display = 'none'
-        document.getElementById('content').style.display = 'block'
-        hiddenMenu.style.display = 'flex'
+        closeContents()
+        profile.style.display = 'block'
+        menu.style.display = 'none'
         switch (e) {
             case 'info':
                 document.getElementById('info-content').style.display = 'block'
                 break;
+            case 'photos':
+                document.getElementById('photos-content').style.display = 'block'
+                break
+            case 'booking':
+                document.getElementById('booking-content').style.display = 'block'
+                break
+            case 'prices':
+                document.getElementById('prices-content').style.display = 'block'
+                break
             default:
                 break;
         }
     }
 
+    closeContents = () => {
+        document.getElementById('info-content').style.display = 'none'
+        document.getElementById('photos-content').style.display = 'none'
+        document.getElementById('booking-content').style.display = 'none'
+        document.getElementById('prices-content').style.display = 'none'
+    }
+
     document.getElementById("info").onclick = () => handleClick('info')
     document.getElementById("hm-info").onclick = () => handleClick('info')
+    document.getElementById("photos").onclick = () => handleClick('photos')
+    document.getElementById("hm-photos").onclick = () => handleClick('photos')
+    document.getElementById("booking").onclick = () => handleClick('booking')
+    document.getElementById("hm-booking").onclick = () => handleClick('booking')
+    document.getElementById("prices").onclick = () => handleClick('prices')
+    document.getElementById("hm-prices").onclick = () => handleClick('prices')
     //show function
 }
