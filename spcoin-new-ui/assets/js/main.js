a16f4fce67a39c5e98286194cca377a296a77777
unknown <realbewhy@gmail.com>
2020-09-28 12:09:49 +0700

    xx

--- spcoin-new-ui/assets/js/main.js
+++ spcoin-new-ui/assets/js/main.js
@@ -1,13 +1,65 @@
-// window.onscroll = function () {
-//   myFunction();
-// };
-// function myFunction() {
-//   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-//     document.getElementById("Menu").style.backgroundColor = "#222f3e";
-//   } else {
-//     document.getElementById("Menu").style.backgroundColor = "transparent";
-//   }
-// }
+fetch("https://medium.com/feed/@spexchange.spcoin")
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then((data) => {
+    const items = data.querySelectorAll("item");
+    items.forEach((item) => {
+      console.log(item);
+      const title = item.querySelector("title").textContent;
+      const link = item.querySelector("guid").textContent;
+      const date = new Date(item.querySelector("updated").textContent);
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+
+      var img = $(
+        item.getElementsByTagNameNS("*", "encoded").item(0).textContent
+      ).find("img")[0].src;
+      console.log(title, link, img, date);
+      $("#swiper-wrapper").append(
+        `<div class="swiper-slide"><div class="card"><img src="` +
+          img +
+          `"><div class="card-body"><a href="` +
+          link +
+          `"><p class="card-text">` +
+          title +
+          `</p></a></div><div class="card-footer"><small class="text-muted">` +
+          monthNames[date.getMonth()] +
+          " " +
+          date.getDate() +
+          ", " +
+          date.getFullYear() +
+          `</small></div></div></div>`
+      );
+    });
+    new Swiper(".swiper-container-news", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      // loop: true,
+      // loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination-news",
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 50,
+        },
+      },
+    });
+  });
 
 window.onload = function () {
   function circle($) {
@@ -50,4 +102,33 @@ window.onload = function () {
   setInterval(() => {
     circle(jQuery);
   }, 2000);
+
+  AOS.init();
+  new Swiper(".swiper-container-roadmap", {
+    slidesPerView: 4,
+    grabCursor: true,
+    activeIndex: 1,
+    breakpoints: {
+      576: { slidesPerView: 1 },
+      767: { slidesPerView: 2 },
+      992: { slidesPerView: 3 },
+    },
+  });
+  new Swiper(".swiper-container-videos", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination-videos",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 50,
+      },
+    },
+  });
+  $(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 };
