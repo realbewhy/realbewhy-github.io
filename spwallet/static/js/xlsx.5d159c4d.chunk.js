f63ea18cee50c7a4639471b9408b436c611c8070
Minh Tam Tran <44467425+realbewhy@users.noreply.github.com>
2022-11-17 21:58:22 +0700

    Delete spwallet directory

--- spwallet/static/js/xlsx.5d159c4d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpspwallet_web=this.webpackJsonpspwallet_web||[]).push([[2],{795:function(n,p){},880:function(n,p){},881:function(n,p){}}]);
-//# sourceMappingURL=xlsx.5d159c4d.chunk.js.map
